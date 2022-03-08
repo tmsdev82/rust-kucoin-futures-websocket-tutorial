9743229c45d368a3ee8f6d95a08db2dde6a7cda2
Tim Steeman <tmdev82@gmail.com>
2022-03-08 21:43:35 +0100

    add logging config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use log::{info};
+use log4rs;
+
 fn main() {
-    println!("Hello, world!");
+    log4rs::init_file("logconfig.yml", Default::default()).expect("Log config file not found.");
+    info!("We now have nice logging!");
 }
