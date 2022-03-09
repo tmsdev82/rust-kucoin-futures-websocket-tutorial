062ba2b867f92a72972192cb9b78164074cfe273
Tim Steeman <tmdev82@gmail.com>
2022-03-09 23:05:22 +0100

    example calling rest api client function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ use log4rs;
 
 mod rest_client;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     log4rs::init_file("logconfig.yml", Default::default()).expect("Log config file not found.");
     info!("We now have nice logging!");
+
+    let client = rest_client::Client::new("https://api-futures.kucoin.com/api/v1".to_string());
+    let result: serde_json::Value = client.post("/bullet-public").await.unwrap();
+
+    info!("KuCoin bullet-public result: {:?}", result);
 }
