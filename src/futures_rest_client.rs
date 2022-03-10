72733f1f0f43030317030c9380c22b8155efeb84
Tim Steeman <tmdev82@gmail.com>
2022-03-10 22:56:23 +0100

    add functions for getting futures contracts symbols and update example in main

--- src/futures_rest_client.rs
+++ src/futures_rest_client.rs
@@ -1,4 +1,7 @@
-use crate::{rest_client::Client, models::{Channels, OpenContracts}};
+use crate::{
+    models::{Channels, OpenContracts},
+    rest_client::Client,
+};
 use anyhow::{bail, Result};
 use log::{debug, error};
 
@@ -20,5 +23,28 @@ impl FuturesRESTClient {
             Ok(channels) => Ok(channels),
             Err(e) => bail!(format!("Error retrieving channels: {:?}", e)),
         }
+    }
+
+    pub async fn get_open_contracts(&self) -> Result<OpenContracts> {
+        let result = self.client.get("/contracts/active", None).await;
+
+        match result {
+            Ok(open_contracts) => Ok(open_contracts),
+            Err(e) => bail!(format!("Error retrieving contracts: {:?}", e)),
+        }
+    }
+
+    pub async fn get_available_symbols(&self) -> Result<Vec<String>> {
+        let contracts = match self.get_open_contracts().await {
+            Ok(contracts) => contracts,
+            Err(e) => {
+                bail!(e);
+            }
+        };
+
+        let symbols_list: Vec<String> = contracts.data.iter().map(|f| f.symbol.clone()).collect();
+        debug!("Found {} available symbols.", symbols_list.len());
+
+        Ok(symbols_list)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ async fn main() {
     log4rs::init_file("logconfig.yml", Default::default()).expect("Log config file not found.");
     info!("We now have nice logging!");
 
-    let client = rest_client::Client::new("https://api-futures.kucoin.com/api/v1".to_string());
-    let result: serde_json::Value = client.post("/bullet-public").await.unwrap();
+    let client = futures_rest_client::FuturesRESTClient::new("https://api-futures.kucoin.com/api/v1");
+    let result = client.get_public_channels().await.unwrap();
 
     info!("KuCoin bullet-public result: {:?}", result);
 }
