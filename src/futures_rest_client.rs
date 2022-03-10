10f7b55c0e2f97a1f189f8c7be195e1baf3c4301
Tim Steeman <tmdev82@gmail.com>
2022-03-10 22:08:24 +0100

    add get_public_channels function for retrieving websocket server urls and connection token

--- src/futures_rest_client.rs
+++ src/futures_rest_client.rs
@@ -1,4 +1,4 @@
-use crate::rest_client::Client;
+use crate::{rest_client::Client, models::{Channels, OpenContracts}};
 use anyhow::{bail, Result};
 use log::{debug, error};
 
@@ -10,6 +10,15 @@ impl FuturesRESTClient {
     pub fn new(hostname: &str) -> FuturesRESTClient {
         FuturesRESTClient {
             client: Client::new(hostname.to_string()),
+        }
+    }
+
+    pub async fn get_public_channels(&self) -> Result<Channels> {
+        let result = self.client.post("/bullet-public").await;
+
+        match result {
+            Ok(channels) => Ok(channels),
+            Err(e) => bail!(format!("Error retrieving channels: {:?}", e)),
         }
     }
 }
