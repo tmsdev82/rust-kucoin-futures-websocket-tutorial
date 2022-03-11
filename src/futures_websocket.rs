3ff0da18e635cec07e97528f8a027c90af9d6cc9
Tim Steeman <tmdev82@gmail.com>
2022-03-11 23:18:56 +0100

    add connect function to futures websocket code

--- src/futures_websocket.rs
+++ src/futures_websocket.rs
@@ -1,8 +1,12 @@
-use crate::futures_rest_client::FuturesRESTClient;
-use serde::{Deserialize, Serialize};
+use crate::{futures_rest_client::FuturesRESTClient, models, websocket};
 use anyhow::{bail, Result};
 use log::{debug, error, info};
+use serde::{Deserialize, Serialize};
+use std::net::TcpStream;
 use std::sync::atomic::{AtomicBool, Ordering};
+use tungstenite::handshake::client::Response;
+use tungstenite::protocol::WebSocket;
+use tungstenite::{stream::MaybeTlsStream, Message};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
@@ -40,12 +44,50 @@ impl FuturesWebSockets {
     }
 
     async fn event_loop(&mut self, running: &AtomicBool) -> Result<()> {
+        info!("Establishing connection...");
+        let mut socket = match self.connect().await {
+            Ok(socket_ok) => socket_ok,
+            Err(error) => {
+                bail!("error: {}", error)
+            }
+        };
+        info!("Connected!");
+        
         info!("Start event loop...");
+        while running.load(Ordering::Relaxed) {}
 
-        while running.load(Ordering::Relaxed) {
-        }
-       
         Ok(())
     }
 
+    async fn connect(&mut self) -> Result<(WebSocket<MaybeTlsStream<TcpStream>>, Response)> {
+        let channels: models::Channels = {
+            debug!("Retrieving channels information from kucoin");
+            let result = self.client.get_public_channels().await;
+
+            match result {
+                Ok(channels) => channels,
+                Err(e) => bail!("Error occurred: {}", e),
+            }
+        };
+
+        debug!(
+            "Loop through {} server(s)",
+            channels.data.instance_servers.len()
+        );
+
+        let mut websocket_urls = Vec::new();
+        for server in channels.data.instance_servers {
+            let connect_id = "_crypto-connector";
+            websocket_urls.push(format!(
+                "{}?token={}&[connectId={}]",
+                &server.endpoint, &channels.data.token, connect_id
+            ));
+        }
+
+        if let Ok(con) = websocket::connect_wss(&self.exchange, websocket_urls) {
+            return Ok(con);
+        }
+
+        bail!("Unable to connect.");
+    }
 }
